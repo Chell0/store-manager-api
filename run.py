1d7253096a542d59bade55c915023770f52bc985
chell0 <machelgabriel@gmail.com>
2018-10-18 07:28:43 +0300

    [ft #161216324] Add resource link for GET request by id

---
+++ run.py
@@ -0,0 +1,20 @@
+# local imports
+from app.v1.views import SaleList, SaleRecord
+
+from flask import Flask
+
+from flask_restful import Api, Resource
+
+
+app = Flask(__name__)  # This will create a Flask wsgi application
+
+api = Api(app)  # This will create a Flask-RESTful API
+
+# This will process a GET and POST request
+api.add_resource(SaleList, '/v1/sales')
+# This will process a GET by Id request
+api.add_resource(SaleRecord, '/v1/sales/<int:id>')
+
+
+if __name__ == '__main__':  # This will start a development server
+    app.run(debug=True)
