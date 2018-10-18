2d7a49b1b17bc0bd45f221b8d7881b82daa286ff
chell0 <machelgabriel@gmail.com>
2018-10-18 15:07:19 +0300

    [Finishes #161310867] Edit views file

--- app/v1/views.py
+++ app/v1/views.py
@@ -1,63 +1,75 @@
-# Here we have all the endpoints
+"""Here we have all the endpoints."""
 from flask import request
 
 from flask_restful import Resource
 
-sales = []
-products = []
+
+SALES = []
+
+PRODUCTS = []
 
 
 # Get all and POST request
 class SaleList(Resource):
+    """Parent class."""
 
-    def get(self):
-        """Fetch all sales recorded"""
-        return sales
+    @classmethod
+    def get(cls):
+        """Fetch all sales recorded."""
+        return SALES
 
-    def post(self):
-        """Create a sale order"""
+    @classmethod
+    def post(cls):
+        """Create a sale order."""
         sale_order = {
             'id': request.json['id'],
             'name': request.json['name'],
             'quantity': request.json['quantity'],
             'price': request.json['price']
         }
-        sales.append(sale_order)
-        return {'sales': sales}, 200
+        SALES.append(sale_order)
+        return {'SALES': SALES}, 200
 
 
 # GET request by Id
 class SaleRecord(Resource):
+    """Parent class."""
 
-    def get(self, id):
-        """Fetch for a single sale order"""
-        sales_order = [sale for sale in sales if sale['id'] == id]
+    @classmethod
+    def get(cls, id):
+        """Fetch for a single sale order."""
+        sales_order = [sale for sale in SALES if sale['id'] == id]
         return {'sale': sales_order[0]}
 
 
 # Get all and POST request
 class ProductList(Resource):
+    """Parent class."""
 
-    def get(self):
-        """Fetch all products"""
-        return products
+    @classmethod
+    def get(cls):
+        """Fetch all products."""
+        return PRODUCTS
 
-    def post(self):
-        """Create a product"""
+    @classmethod
+    def post(cls):
+        """Create a product."""
         add_product = {
             'id': request.json['id'],
             'name': request.json['name'],
             'stock': request.json['stock'],
             'price': request.json['price']
         }
-        products.append(add_product)
-        return {'products': products}, 200
+        PRODUCTS.append(add_product)
+        return {'products': PRODUCTS}, 200
 
 
 # GET request by Id
 class OneProduct(Resource):
+    """Parent class."""
 
-    def get(self, id):
-        """Fetch for a single product"""
-        get_one = [product for product in products if product['id'] == id]
-        return {'product': get_one[0]}
+    @classmethod
+    def get(cls, id):
+        """Fetch for a single product."""
+        getone = [product for product in PRODUCTS if product['id'] == id]
+        return {'product': getone[0]}
