5b463929519065f56cb3331b659add21e2166c54
chell0 <machelgabriel@gmail.com>
2018-10-18 06:02:20 +0300

    [ft #161216230] Edit run.py

---
+++ run.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+# local imports
+from app.v1.views import SaleList
+
+
+app = Flask(__name__)  # This will create a Flask wsgi application
+api = Api(app)  # This will create a Flask-RESTful API
+
+# This will process a GET and POST request
+api.add_resource(SaleList, '/v1/sales')
+
+
+if __name__ == '__main__':  # This will start a development server
+    app.run(debug=True)
