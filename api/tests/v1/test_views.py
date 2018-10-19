7482ebd0bb193acb996ae79b6a533802f3c22e70
chell0 <machelgabriel@gmail.com>
2018-10-19 14:58:26 +0300

    [ch #161214201] Add test file and Travis file as well

--- api/tests/v1/test_views.py
+++ api/tests/v1/test_views.py
@@ -2,5 +2,16 @@ import unittest
 import json
 
 # Local imports
-from api.app.v1 import views
+from api.tests.v1.test_base_case import BaseTest
 
+
+class TestFlaskApi(BaseTest):
+    """Testing out Flask Api."""
+
+    def test_get_all_sales(self):
+        """Test fetching all sales."""
+        mac = self.client.get(
+            '/v1/sales',
+            content_type="application/json"
+        )
+        self.assertEqual(mac.status_code, 200)
