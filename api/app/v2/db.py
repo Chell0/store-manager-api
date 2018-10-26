bbb828873c4f95861f21bbdad3a6aea5b2698a70
chell0 <machelgabriel@gmail.com>
2018-10-26 11:51:21 +0300

    [ft #161407484] Add database connection

---
+++ api/app/v2/db.py
@@ -0,0 +1,10 @@
+import os
+import psycopg2
+
+
+class StoreDb:
+    def __init__(self):
+        self.connection = psycopg2.connect(os.getenv("DATABASE_URL"))
+        self.cursor = self.connection.cursor()
+        self.connection.commit()
+        self.cursor.close()
