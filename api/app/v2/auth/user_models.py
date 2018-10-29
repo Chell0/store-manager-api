86c4a5a51b344b36fe7f8081bd73957b00a17a38
chell0 <machelgabriel@gmail.com>
2018-10-29 06:21:59 +0300

    [ft #161407484] Add user registration

--- api/app/v2/auth/user_models.py
+++ api/app/v2/auth/user_models.py
@@ -2,6 +2,7 @@
 from api.app.v2.db import StoreDb
 
 from werkzeug.exceptions import NotFound, BadRequest
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class Authentication(StoreDb):
@@ -24,18 +25,40 @@ class Authentication(StoreDb):
         self.connection.commit()
         return user_id
 
-    def login(self, data):
+    def fetch_email(self, data):
         """Login user."""
-        self.cursor.execute(
-            """
-            SELECT * FROM users WHERE email='%s'
-            """
-            % (data['email'])
+        self.cursor.execute("SELECT * FROM users WHERE email='%s'"
+            % ('email')
         )
         result = self.cursor.fetchone()[0]
         if not result:
-            raise NotFound("Email not found")
+            raise NotFound("Email was not found")
         if result[3] == data['password']:
             return data
         else:
-            raise BadRequest("password do not match")
+            raise BadRequest("Password does not match")
+
+    def get_user_by_id(self, user_id):
+        """Fetch a specific user by id."""
+        self.cursor.execute("SELECT * FROM users WHERE user_id='%s'"
+            % (user_id)
+        )
+        result = self.cursor.fetchone()
+        return result
+
+    def is_admin_present(self):
+        """Check is an admin is present."""
+        self.cursor.execute("SELECT * FROM users WHERE is_admin=True"
+        )
+        result = self.cursor.fetchone()
+        return result
+
+    def give_admin_rights(self, email):
+        """Give admin rights to a normal user."""
+        self.cursor.execute(
+            """UPDATE users SET is_admin=True WHERE email='%s'
+            """
+            % (email)
+        )
+        self.connection.commit()
+        return email
--- api/app/v2/auth/user_views.py
+++ api/app/v2/auth/user_views.py
@@ -1,12 +1,94 @@
 from flask_restful import Resource, reqparse
 
 # local imports
+from api.app.v2.security.validate import Validate
 from api.app.v2.auth.user_models import Authentication
+validate = Validate()
 usr = Authentication()
 
 
-class Login(Resource):
-    """Login class."""
+class UserRegistration(Resource):
+	"""User registration class."""
 
-    def add_user(self):
-        usr.create_user()
+	def post(self):
+		parser = reqparse.RequestParser()
+		parser.add_argument('user_name',
+			type=str,
+			required=True,
+			help="Username cannot be left blank!"
+		)
+		parser.add_argument('email',
+			type=str,
+			required=True,
+			help="Email cannot be left blank!"
+		)
+		parser.add_argument('password',
+			type=str,
+			required=True,
+			help="Password cannot be left blank!"
+		)
+		args = parser.parse_args()
+		email = validate.validate_email(args['email'])
+		password = validate.validate_password(args['password'])
+		user = usr.fetch_email(email)
+
+
+class UserLogin(Resource):
+	"""User login class."""
+	def post(self):
+		parser = reqparse.RequestParser()
+		parser.add_argument('email',
+			type=str,
+			required=True,
+			help="Email cannot be left blank!"
+		)
+		parser.add_argument('password',
+			type=str,
+			required=True,
+			help="Password cannot be left blank!"
+		)
+		args=parser.parse_args()
+		email = validate.validate_email(args['email'])
+		password = validate.validate_password(args['password'])
+
+
+class CreateAttendantAccount(Resource):
+	"""User store attendant account creation."""
+
+	def post(self):
+		parser = reqparse.RequestParser()
+		parser.add_argument('user_name',
+			type=str,
+			required=True,
+			help="Username cannot be left blank!"
+		)
+		parser.add_argument('email',
+			type=str,
+			required=True,
+			help="Email cannot be left blank!"
+		)
+		parser.add_argument('password',
+			type=str,
+			required=True,
+			help="Password cannot be left blank!"
+		)
+		args = parser.parse_args()
+		email = validate.validate_email(args['email'])
+		password = validate.validate_password(args['password'])
+
+
+class GiveAdminRights(Resource):
+	"""Admin rights class."""
+
+	def put(self):
+		parser = reqparse.RequestParser()
+		parser.add_argument('email',
+			type=str,
+			required=True,
+			help="Please provide an email address for an update to occur"
+		)
+		args = parser.parse_args()
+		email = validate.validate_email(args['email'])
+
+
+
